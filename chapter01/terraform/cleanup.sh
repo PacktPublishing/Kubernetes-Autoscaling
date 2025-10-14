71ff711101f32fc6ec5a07bb14292e6080b7693b
Christian Melendez <christianhxc@gmail.com>
2025-10-14 14:05:45 +0000

    cleaned the cleanup script

--- chapter01/terraform/cleanup.sh
+++ chapter01/terraform/cleanup.sh
@@ -3,9 +3,6 @@
 echo "Destroying the Amazon EKS cluster ... "
 
 kubectl delete --all svc
-kubectl delete --all nodeclaim
-kubectl delete --all nodepool
-kubectl delete --all ec2nodeclass
 export TF_VAR_region=eu-west-1 # Change it to $AWS_REGION if you want to use the one provided from the environment
 terraform destroy -target="module.eks_blueprints_addons" --auto-approve
 terraform destroy -target="module.eks" --auto-approve
