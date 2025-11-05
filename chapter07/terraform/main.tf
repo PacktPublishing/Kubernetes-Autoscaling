2f5cb3014367b9ea6e90b1268ad77628540a937b
Christian Melendez <christianhxc@gmail.com>
2025-11-05 23:21:18 +0000

    updated Terraform template for Chapter 7

--- chapter07/terraform/main.tf
+++ chapter07/terraform/main.tf
@@ -1,33 +1,34 @@
 terraform {
-  required_version = ">= 1.0.0"
+  required_version = ">= 1.3.2"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.72"
+      version = "~> 6.16"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.10"
+      version = ">= 2.30"
     }
     helm = {
       source  = "hashicorp/helm"
-      version = "= 2.11.0"
+      version = "~> 2.17"
     }
     kubectl = {
       source  = "alekc/kubectl"
-      version = ">= 2.0.2"
+      version = ">= 2.1"
     }
   }
 }
 
+## THIS TO AUTHENTICATE TO ECR, DON'T CHANGE IT
 provider "aws" {
   region = "us-east-1"
   alias  = "virginia"
 }
 
 provider "aws" {
-  region = local.region
+  region = var.region
 }
 
 provider "kubernetes" {
@@ -60,100 +61,56 @@ data "aws_ecrpublic_authorization_token" "token" {
   provider = aws.virginia
 }
 
-data "aws_partition" "current" {}
-data "aws_caller_identity" "current" {}
-data "aws_availability_zones" "available" {}
-
-variable "region" {
-  description = "Region to deploy the resources"
-  type        = string
+data "aws_availability_zones" "available" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
 }
 
 locals {
-  name   = "kubernetes-autoscaling"
-  region = var.region
-
-  cluster_version = "1.31"
+  name = "kubernetes-autoscaling"
 
   vpc_cidr = "10.0.0.0/16"
-  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
+  # NOTE: You might need to change this less number of AZs depending on the region you're deploying to
+  azs = slice(data.aws_availability_zones.available.names, 0, 3)
 
   tags = {
-    Blueprint = local.name
+    blueprint = local.name
   }
 }
 
 ################################################################################
 # Cluster
 ################################################################################
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "20.8.3"
+  version = "21.3.2"
 
-  cluster_name                   = local.name
-  cluster_version                = local.cluster_version
-  cluster_endpoint_public_access = true
-
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
-
-  create_cloudwatch_log_group              = false
-  create_cluster_security_group            = false
-  create_node_security_group               = false
-  authentication_mode                      = "API_AND_CONFIG_MAP"
+  name                                     = local.name
+  kubernetes_version                       = var.eks_version
+  endpoint_public_access                   = true
   enable_cluster_creator_admin_permissions = true
 
-  eks_managed_node_groups = {
-    managed-ondemand = {
-      node_group_name = "managed-ondemand"
-      instance_types  = ["m6g.xlarge", "m6gd.xlarge", "m7g.xlarge", "c6g.xlarge", "c6gd.xlarge", "c7g.xlarge"]
-
-      create_security_group = false
-
-      subnet_ids   = module.vpc.private_subnets
-      max_size     = 10
-      desired_size = 2
-      min_size     = 2
-
-      # Launch template configuration
-      ami_type               = "BOTTLEROCKET_ARM_64"
-      create_launch_template = true # false will use the default launch template
-      launch_template_os     = "bottlerocket"
-
-      labels = {
-        intent = "control-apps"
-      }
-    }
-  }
-
-  tags = merge(local.tags, {
-    "karpenter.sh/discovery" = local.name
-  })
-}
-
-module "eks_blueprints_addons" {
-  source  = "aws-ia/eks-blueprints-addons/aws"
-  version = "1.16.3"
-
-  cluster_name      = module.eks.cluster_name
-  cluster_endpoint  = module.eks.cluster_endpoint
-  cluster_version   = module.eks.cluster_version
-  oidc_provider_arn = module.eks.oidc_provider_arn
-
-  create_delay_dependencies = [for prof in module.eks.eks_managed_node_groups : prof.node_group_arn]
-
-  enable_metrics_server = true
-
-  enable_cluster_autoscaler = false
-
-  eks_addons = {
+  addons = {
     aws-ebs-csi-driver = {
-      service_account_role_arn = module.ebs_csi_driver_irsa.iam_role_arn
+      most_recent = true
+      pod_identity_association = [{
+        role_arn        = module.aws_ebs_csi_pod_identity.iam_role_arn
+        service_account = "ebs-csi-controller-sa"
+      }]
     }
-
-    kube-proxy = { most_recent = true }
-    coredns    = { most_recent = true }
-
+    coredns = {
+      most_recent = true
+    }
+    eks-pod-identity-agent = {
+      before_compute = true
+      most_recent    = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
     vpc-cni = {
       most_recent    = true
       before_compute = true
@@ -164,70 +121,72 @@ module "eks_blueprints_addons" {
         }
       })
     }
+    eks-node-monitoring-agent = {
+      most_recent = true
+    }
   }
 
-  tags = local.tags
-}
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
 
-module "eks_blueprints_addons_load_balancer_controller" {
-  source  = "aws-ia/eks-blueprints-addons/aws"
-  version = "1.16.3"
+  create_cloudwatch_log_group = false
 
-  cluster_name      = module.eks.cluster_name
-  cluster_endpoint  = module.eks.cluster_endpoint
-  cluster_version   = module.eks.cluster_version
-  oidc_provider_arn = module.eks.oidc_provider_arn
+  eks_managed_node_groups = {
+    mng = {
+      instance_types = ["m4.large", "m5.large", "m5a.large", "m5ad.large", "m5d.large", "t2.large", "t3.large", "t3a.large"]
 
-  enable_aws_load_balancer_controller = true
-  aws_load_balancer_controller = {
-    chart_version = "1.8.2"
-  }
+      subnet_ids   = module.vpc.private_subnets
+      max_size     = 10
+      desired_size = 2
+      min_size     = 2
 
-  tags = local.tags
-
-  depends_on = [module.eks_blueprints_addons]
-}
-
-module "ebs_csi_driver_irsa" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-  version = "5.44.0"
-
-  role_name_prefix = "${module.eks.cluster_name}-ebs-csi-driver-"
-
-  attach_ebs_csi_policy = true
-
-  oidc_providers = {
-    main = {
-      provider_arn               = module.eks.oidc_provider_arn
-      namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
+      labels = {
+        "CriticalAddonsOnly" = "true"
+      }
     }
   }
 
   tags = local.tags
 }
 
-################################################################################
-# Storage Classes
-################################################################################
+module "eks_blueprints_addons" {
+  source  = "aws-ia/eks-blueprints-addons/aws"
+  version = "1.22.0"
 
-resource "kubernetes_annotations" "gp2" {
-  api_version = "storage.k8s.io/v1"
-  kind        = "StorageClass"
-  # This is true because the resources was already created by the ebs-csi-driver addon
-  force = "true"
+  cluster_name      = module.eks.cluster_name
+  cluster_endpoint  = module.eks.cluster_endpoint
+  cluster_version   = module.eks.cluster_version
+  oidc_provider_arn = module.eks.oidc_provider_arn
 
-  metadata {
-    name = "gp2"
+  create_delay_dependencies = [for grp in module.eks.eks_managed_node_groups : grp.node_group_arn]
+
+  enable_aws_load_balancer_controller = true
+  enable_metrics_server               = true
+  
+  enable_cluster_autoscaler = false
+
+  aws_load_balancer_controller = {
+    values = [
+      <<-EOT
+      region: ${var.region}
+      vpcId: ${module.vpc.vpc_id}
+      clusterName: ${module.eks.cluster_name}
+      EOT
+    ]
   }
 
-  annotations = {
-    # Modify annotations to remove gp2 as default storage class still retain the class
-    "storageclass.kubernetes.io/is-default-class" = "false"
-  }
+  tags = local.tags
+}
 
-  depends_on = [
-    module.eks_blueprints_addons
-  ]
+module "aws_ebs_csi_pod_identity" {
+  source = "terraform-aws-modules/eks-pod-identity/aws"
+
+  name    = "aws-ebs-csi"
+  version = "2.2.1"
+
+  attach_aws_ebs_csi_policy = true
+
+  tags = local.tags
 }
 
 resource "kubernetes_storage_class_v1" "gp3" {
@@ -262,14 +221,14 @@ resource "kubernetes_storage_class_v1" "gp3" {
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "5.6.0"
+  version = "5.21.0"
 
   name = local.name
   cidr = local.vpc_cidr
 
   azs             = local.azs
-  public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k)]
-  private_subnets = ["10.0.32.0/19", "10.0.64.0/19", "10.0.96.0/19"]
+  private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k)]
+  public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 48)]
 
   enable_nat_gateway   = true
   single_nat_gateway   = true
@@ -291,30 +250,33 @@ module "vpc" {
   private_subnet_tags = {
     "kubernetes.io/cluster/${local.name}" = "shared"
     "kubernetes.io/role/internal-elb"     = 1
-    "karpenter.sh/discovery"              = local.name
   }
 
   tags = local.tags
 }
 
-resource "aws_s3_bucket" "codepipeline_eks_bucket" {
-  bucket = "${local.region}-${data.aws_caller_identity.current.account_id}-${local.name}-codepipeline-eks"
+variable "region" {
+  description = "Region to deploy the resources"
+  type        = string
 }
 
-resource "aws_s3_bucket_public_access_block" "codepipeline_eks_bucket_pab" {
-  bucket = aws_s3_bucket.codepipeline_eks_bucket.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+variable "eks_version" {
+  description = "Amazon EKS version to use"
+  type        = string
+  default     = "1.34"
 }
-
-#---------------------------------------------------------------
-# Outputs
-#---------------------------------------------------------------
 
 output "configure_kubectl" {
   description = "Configure kubectl: make sure you're logged in with the correct AWS profile and run the following command to update your kubeconfig"
-  value       = "aws eks --region ${local.region} update-kubeconfig --name ${module.eks.cluster_name}"
+  value       = "aws eks --region ${var.region} update-kubeconfig --name ${module.eks.cluster_name}"
+}
+
+output "cluster_name" {
+  description = "Cluster name of the EKS cluster"
+  value       = module.eks.cluster_name
+}
+
+output "vpc_id" {
+  description = "VPC ID that the EKS cluster is using"
+  value       = module.vpc.vpc_id
 }
